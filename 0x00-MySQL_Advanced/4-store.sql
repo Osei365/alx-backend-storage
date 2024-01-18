0d1e5bee764d26c33ad57ed3accf56164980330f
Osei <williamsabodunrin@gmail.com>
2024-01-19 00:28:21 +0100

    Update 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,6 @@
 -- store
-DELIMITER $$
-CREATE TRIGGER IF NOT EXISTS reduce_quantity
-AFTER INSERT ON orders
+CREATE TRIGGER decrement
+AFTER INSERT
+ON orders
 FOR EACH ROW
-BEGIN
-    UPDATE items
-        SET quantity = quantity - NEW.number
-        WHERE name = NEW.item_name;
-END $$
-DELIMITER ;
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
