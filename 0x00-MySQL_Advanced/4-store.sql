c00816b31a1d3f89d13a6a80c995fcb3994e2ca2
Osei <williamsabodunrin@gmail.com>
2024-01-19 00:24:52 +0100

    store

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- store
+DELIMITER $$
+CREATE TRIGGER IF NOT EXISTS reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
