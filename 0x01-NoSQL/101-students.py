6a34bfac18b4ab497c524c3e1a210b6f93acbdb7
osei <williamsabodunrin@gmail.com>
2024-01-23 16:09:04 -0800

    aggregates

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''module 101-students
+'''
+
+
+def top_students(mongo_collection):
+    '''aggregates average score'''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
