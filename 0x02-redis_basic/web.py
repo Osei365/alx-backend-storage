10e549649b67ca3d052af3c012944d55d24cb322
Osei <williamsabodunrin@gmail.com>
2024-01-25 03:17:40 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,23 +10,24 @@ from typing import Callable
 client = redis.Redis()
 
 
-def getpage_deco(func: Callable) -> Callable:
-    '''decorates the get_page.'''
-    @wraps(func)
-    def wrapper(url) -> str:
-        '''wraps itself around func.'''
-        client.incr(f"count:{url}")
-        result = client.get(f"result:{url}")
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
-        result = func(url)
-        client.set(f"count:{url}", 0)
-        client.setex(f"result:{url}", 10, result)
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
-    return wrapper
+    return invoker
 
-
-@getpage_deco
+@data_catcher
 def get_page(url: str) -> str:
     '''tracks number of times a url is accessed'''
     return requests.get(url).text
